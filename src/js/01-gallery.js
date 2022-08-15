a070471b727a41634b9d07583f1358bcfc1b0bfa
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-15 14:53:48 +0300

    Change add library

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,9 @@
+import SimpleLightbox from "simplelightbox";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
