1f0b91bfd39d4cf3c4276424e2114d9ab12600dd
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-18 18:38:56 +0300

    Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,42 @@
 import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(SimpleLightbox);
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
 
+const galleryList = document.querySelector('.gallery');
+const image = document.querySelector('.gallery__image');
 
 
+function galleryListMarkup() {
+    return galleryItems.map(({ preview, original, description }) => {
+ return `<li class = gallery_lists>
+         <a class = gallery__item href='${original}'>
+      <img
+      class= 'gallery__image'
+      src= '${preview}'      
+      alt='${description}'
+    />
+  </a>
+</li>`})
+        .join('');
+    
+};
+
+const markupFunEl = galleryListMarkup(galleryItems);
+galleryList.insertAdjacentHTML('afterbegin', markupFunEl);
+
+const optionsLibrary = {
+    overlay: true,
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom',
+    enableKeyboard: true,
+};
+let gallery = new SimpleLightbox('.gallery a', optionsLibrary);
+console.log(gallery);
