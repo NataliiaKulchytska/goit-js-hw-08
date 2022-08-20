82e309c9e0c92a88dee5bf809bfa43986903ce11
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-20 10:11:46 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+
+
+const onPlay = function (data) {
+    const stringJSON = JSON.stringify(data);
+    localStorage.setItem('videoplayer-current-time', stringJSON)
+    console.log(onPlay);
+};
+
+
+const iframe = document.querySelector('ifraime');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(onPlay, 500));
+player.setCurrentTime(getCurrentTime());
+
+const getCurrentTime = function () {
+    const parsJSON = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+
+    return parsJSON.seconds;
+};
+
 
