8ce2dfabc1aefc689836a735733c6d513931aa90
NataliiaKulchytska <kylchitskaia@gmail.com>
2022-08-22 21:04:58 +0300

    change task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,17 @@
 import throttle from 'lodash.throttle';
 
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    input: document.querySelector('[name = email]'),
-    text: document.querySelector('[name = message]'),
-};
+const form = document.querySelector('.feedback-form');
 
 const KEY_FORM = 'feedback-form-state';
 const infoForm = {};
 
 
-refs.form.addEventListener('input', throttle(onInput, 500));
-refs.form.addEventListener('submit', onSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+form.addEventListener('submit', onSubmit);
 
 
-function onInput(e) {
-    // const value = refs.input.value;
-
-    // console.log();
+function onInput(e) {        
+   
     infoForm[e.target.name] = e.target.value;
 
     const infoSave = JSON.stringify(infoForm);
@@ -28,30 +22,31 @@ function onInput(e) {
 function onSubmit(e) {
      e.preventDefault();  
     const localSave = localStorage.getItem('KEY_FORM');
-        // infoForm[e.target.name] = e.target.value;
-
-    // const infoSave = JSON.stringify(infoForm);
-    // localStorage.setItem('KEY_FORM', infoSave);
+       
     try {
-        // const localObject = JSON.parse(localSave);
+       
         const localData = localSave ? JSON.parse(localSave) : {};
-        // onInput(e);
+        
         console.log(localData);
     } catch (error) {
 console.log('parsing error');
     }    
     
-    //  console.log(localSave); 
-    // localStorage.removeItem(KEY_FORM);
+    
+    localStorage.removeItem(KEY_FORM);
     e.target.reset();
 };
 
 
-// function onSave() {
-//     const localSave = localStorage.getItem('KEY_FORM');
-//     try {
-//         const savedForm = JSON.parse('KEY_FORM');
-//         console.log(savedForm);
-        
-//     }catch (error){}
-// };
+function initForm() {
+  let save = localStorage.getItem('KEY_FORM');
+  if (save) {
+    save = JSON.parse(save);
+    Object.entries(save).forEach(([name, value]) => {
+      onInput.elements[name].value = value;
+    });
+  }
+};
+console.log(initForm);
+
+
